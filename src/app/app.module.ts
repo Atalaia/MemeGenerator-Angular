6bee78747d5b0819c59f2687abf3228180b17e26
Maria Soage <maria.soage01@gmail.com>
2020-08-13 18:30:56 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ColorChromeModule } from 'ngx-color/chrome';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GeneratorComponent
+  ],
+  imports: [
+    BrowserModule,
+    ColorChromeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
